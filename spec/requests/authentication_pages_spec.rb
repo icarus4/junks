3a1673d521ede8b02db93d0043c19745f12a6e86
icarus4 <icarus4.chu@gmail.com>
2014-03-28 20:36:17 +0800

    Finish sign in/out

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -13,8 +13,13 @@ describe "Authentication" do
   describe "signin" do
     before { visit signin_path }
 
-    it { should have_title('Sign in') }
-    it { should have_selector('div.alert.alert-error') }
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_title('Sign in') }
+      it { should have_selector('div.alert.alert-error') }
+    end
+
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
